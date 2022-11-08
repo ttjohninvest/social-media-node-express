c4c9e4070108a714b3c8fcb4e83912b646495d8b
Shlomi Nugarker <shlomin1231@gmail.com>
2022-11-09 01:54:26 +0200

    minor fix

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-  dbURL: '',
+  dbURL:
+    'mongodb+srv://shlomin1231:R3MwCaU6dbdMb16U@cluster0.ysm5t.mongodb.net/social_network_db?retryWrites=true&w=majority',
 
   // dbURL: 'mongodb://localhost:27017',
 }
