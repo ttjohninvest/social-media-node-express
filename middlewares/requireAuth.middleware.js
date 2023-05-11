c14d309f93ebf62b6c6cf0f8bd5dcf149f53fe15
Shlomi Nugarker <shlomin1231@gmail.com>
2023-05-11 11:55:58 +0300

    requireAuth

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -1,10 +1,10 @@
 const logger = require('../services/logger.service')
 
 async function requireAuth(req, res, next) {
-  // if (!req.session || !req.session.user) {
-  //   res.status(401).end('Unauthorized!')
-  //   return
-  // }
+  if (!req.session || !req.session.user) {
+    res.status(401).end('Unauthorized!')
+    return
+  }
   next()
 }
 
@@ -19,7 +19,7 @@ async function requireAdmin(req, res, next) {
   next()
 }
 
-// module.exports = requireAuth
+module.exports = requireAuth
 
 module.exports = {
   requireAuth,
